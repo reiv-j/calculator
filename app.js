c75eadff608be09091afb55a7785567dec327141
reiv <javierromaynieto@gmail.com>
2024-02-05 13:00:00 +0100

    Create main operation functions

---
+++ app.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operation) {
+    if (operation === "add") {
+        return add(num1, num2);
+    } else if (operation === "subtract") {
+        return subtract(num1, num2);
+    } else if (operation === "multiply") {
+        return multiply(num1, num2);
+    } else if (operation === "divide") {
+        return divide(num1, num2);
+    }
+}
